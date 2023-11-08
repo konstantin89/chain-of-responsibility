c1e9a748c21fecbe440a802a76503cdc38c8e2c4
Kosta Bulgakov <kosta.bulgakov@mail.huji.ac.il>
2023-11-08 16:38:37 +0200

    minimal implemenation

---
+++ tests/TestUtils.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+
+#include "Chain.h"
+
+
+// ------------------- Number processing chain --------------------
+
+class Number
+{
+public:
+
+    Number(int number) : mNumber(number) { }
+    int& GetValue() { return mNumber; }
+
+    bool isEven = false;
+    bool isPositive = false;
+
+private:
+    int mNumber;
+};
+
+class IsNumberEvenLink : public chain_of_responsibility::Link<Number>
+{
+public:
+
+    using ProcessingResult = typename chain_of_responsibility::Link<Number>::ProcessingResult;
+
+    ProcessingResult Process(DataRefType data) override
+    {
+
+        data.isEven = !(data.GetValue() % 2);
+        return ProcessingResult::Continue;
+    }
+};
+
+class IsNumberPositiveLink : public chain_of_responsibility::Link<Number>
+{
+public:
+
+    using ProcessingResult = typename chain_of_responsibility::Link<Number>::ProcessingResult;
+
+    ProcessingResult Process(DataRefType data) override
+    {
+        data.isPositive = data.GetValue() >= 0;
+        return ProcessingResult::Continue;
+    }
+};
+
+// ------------------- Text processing chain --------------------
+class Text
+{
+public:
+    using TextPtrType = std::shared_ptr<Text>;
+
+    Text(std::string text) : mText(text) {};
+    std::string& GetText() { return mText; }
+
+    long numberOfLines = 0;
+    long numberOfCharacters = 0;
+
+private:
+    std::string mText;
+};
+
+class TextLinesCounterLink : public chain_of_responsibility::Link<Text::TextPtrType>
+{
+    using ProcessingResult = typename chain_of_responsibility::Link<Text::TextPtrType>::ProcessingResult;
+
+    ProcessingResult Process(DataRefType data) override
+    {
+        auto text = data->GetText();
+        
+        data->numberOfLines = std::count_if(text.begin(), text.end(), [](char c)
+        {
+            return (c == '\n');
+        });
+
+        if (data->numberOfLines) data->numberOfLines++;
+
+        return ProcessingResult::Continue;
+    }
+};
+
+class TextCharacterCounterLink : public chain_of_responsibility::Link<Text::TextPtrType>
+{
+    using ProcessingResult = typename chain_of_responsibility::Link<Text::TextPtrType>::ProcessingResult;
+
+    ProcessingResult Process(DataRefType data) override
+    {
+        auto text = data->GetText();
+        data->numberOfCharacters = text.length();
+        return ProcessingResult::Continue;
+    }
+};
